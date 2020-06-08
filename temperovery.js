37c5dbfae4795bdfef3fc3e2e52386f576310594
rajaKumaranraja <p.rajakumaran20@gmail.com>
2020-06-08 10:59:44 +0530

    Story 182: fix benchmarks for speed

---
+++ temperovery.js
@@ -0,0 +1,14 @@
+On branch master
+Your branch is ahead of 'origin/master' by 2 commits.
+  (use "git push" to publish your local commits)
+
+Changes not staged for commit:
+  (use "git add/rm <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	deleted:    CONTRIBUTING.md
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.CONTRIBUTING.md.swp
+
+no changes added to commit (use "git add" and/or "git commit -a")
